ccff6ed42dfb7c55529d05ba4c456b873a541271
ephilip68 <erwin.philip68@gmail.com>
2024-11-03 21:17:46 +0100

    exo gaulois - sql

---
+++ datagaulois.sql
@@ -0,0 +1,148 @@
+LastSessions<|||>1<|||>localhost
+LastActiveSession<|||>1<|||>localhost
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,298
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+MainWinWidth<|||>3<|||>993
+MainWinHeight<|||>3<|||>525
+SessionManager_WindowLeft<|||>3<|||>2307
+SessionManager_WindowTop<|||>3<|||>550
+CreateDbCollation<|||>1<|||>latin1_swedish_ci
+ColWidths_frmTableEditor.listColumns<|||>1<|||>58,100,154,90,60,65,50,117,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>298,100,80,120
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>1118,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,404
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+SQLFile0<|||>1<|||>C:\laragon\bin\heidisql\Backups\query-tab-2024-11-03_14-46-19-943.sql
+Servers\Laragon.MySQL\SessionCreated<|||>1<|||>2022-09-09 20:36:07
+Servers\Laragon.MySQL\Host<|||>1<|||>127.0.0.1
+Servers\Laragon.MySQL\WindowsAuth<|||>3<|||>0
+Servers\Laragon.MySQL\CleartextPluginEnabled<|||>3<|||>0
+Servers\Laragon.MySQL\User<|||>1<|||>root
+Servers\Laragon.MySQL\Password<|||>1<|||>2
+Servers\Laragon.MySQL\LoginPrompt<|||>3<|||>0
+Servers\Laragon.MySQL\Port<|||>1<|||>3306
+Servers\Laragon.MySQL\NetType<|||>3<|||>0
+Servers\Laragon.MySQL\Compressed<|||>3<|||>0
+Servers\Laragon.MySQL\LocalTimeZone<|||>3<|||>0
+Servers\Laragon.MySQL\QueryTimeout<|||>3<|||>30
+Servers\Laragon.MySQL\KeepAlive<|||>3<|||>20
+Servers\Laragon.MySQL\FullTableStatus<|||>3<|||>1
+Servers\Laragon.MySQL\Databases<|||>1<|||>
+Servers\Laragon.MySQL\Library<|||>1<|||>libmysql.dll
+Servers\Laragon.MySQL\Comment<|||>1<|||>
+Servers\Laragon.MySQL\StartupScriptFilename<|||>1<|||>
+Servers\Laragon.MySQL\TreeBackground<|||>3<|||>536870911
+Servers\Laragon.MySQL\SshExecutable<|||>1<|||>plink.exe
+Servers\Laragon.MySQL\SSHtunnelHost<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelHostPort<|||>3<|||>22
+Servers\Laragon.MySQL\SSHtunnelUser<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelPassword<|||>1<|||>2
+Servers\Laragon.MySQL\SSHtunnelTimeout<|||>3<|||>4
+Servers\Laragon.MySQL\SSHtunnelPrivateKey<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelPort<|||>3<|||>3307
+Servers\Laragon.MySQL\SSL_Active<|||>3<|||>0
+Servers\Laragon.MySQL\SSL_Key<|||>1<|||>
+Servers\Laragon.MySQL\SSL_Cert<|||>1<|||>
+Servers\Laragon.MySQL\SSL_CA<|||>1<|||>
+Servers\Laragon.MySQL\SSL_Cipher<|||>1<|||>
+Servers\Laragon.MySQL\IgnoreDatabasePattern<|||>1<|||>
+Servers\Laragon.MySQL\LogFileDdl<|||>3<|||>0
+Servers\Laragon.MySQL\LogFileDml<|||>3<|||>0
+Servers\Laragon.MySQL\LogFilePath<|||>1<|||>C:\Users\Administrator\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\Laragon.MySQL\RefusedCount<|||>3<|||>3
+Servers\Laragon.MySQL\ServerVersionFull<|||>1<|||>8.0.30 - MySQL Community Server - GPL
+Servers\Laragon.MySQL\ConnectCount<|||>3<|||>3
+Servers\Laragon.MySQL\ServerVersion<|||>3<|||>80030
+Servers\Laragon.MySQL\LastConnect<|||>1<|||>2022-09-09 20:37:19
+Servers\localhost\SessionCreated<|||>1<|||>2024-11-03 14:44:05
+Servers\localhost\Host<|||>1<|||>127.0.0.1
+Servers\localhost\WindowsAuth<|||>3<|||>0
+Servers\localhost\CleartextPluginEnabled<|||>3<|||>0
+Servers\localhost\User<|||>1<|||>root
+Servers\localhost\Password<|||>1<|||>1
+Servers\localhost\LoginPrompt<|||>3<|||>0
+Servers\localhost\Port<|||>1<|||>3306
+Servers\localhost\NetType<|||>3<|||>0
+Servers\localhost\Compressed<|||>3<|||>0
+Servers\localhost\LocalTimeZone<|||>3<|||>0
+Servers\localhost\QueryTimeout<|||>3<|||>30
+Servers\localhost\KeepAlive<|||>3<|||>20
+Servers\localhost\FullTableStatus<|||>3<|||>1
+Servers\localhost\Databases<|||>1<|||>
+Servers\localhost\Library<|||>1<|||>libmariadb.dll
+Servers\localhost\Comment<|||>1<|||>
+Servers\localhost\StartupScriptFilename<|||>1<|||>
+Servers\localhost\TreeBackground<|||>3<|||>536870911
+Servers\localhost\SshExecutable<|||>1<|||>
+Servers\localhost\SSHtunnelHost<|||>1<|||>
+Servers\localhost\SSHtunnelHostPort<|||>3<|||>22
+Servers\localhost\SSHtunnelUser<|||>1<|||>
+Servers\localhost\SSHtunnelPassword<|||>1<|||>1
+Servers\localhost\SSHtunnelTimeout<|||>3<|||>4
+Servers\localhost\SSHtunnelPrivateKey<|||>1<|||>
+Servers\localhost\SSHtunnelPort<|||>3<|||>3307
+Servers\localhost\SSL_Active<|||>3<|||>0
+Servers\localhost\SSL_Key<|||>1<|||>
+Servers\localhost\SSL_Cert<|||>1<|||>
+Servers\localhost\SSL_CA<|||>1<|||>
+Servers\localhost\SSL_Cipher<|||>1<|||>
+Servers\localhost\IgnoreDatabasePattern<|||>1<|||>
+Servers\localhost\LogFileDdl<|||>3<|||>0
+Servers\localhost\LogFileDml<|||>3<|||>0
+Servers\localhost\LogFilePath<|||>1<|||>C:\Users\pario\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\localhost\RefusedCount<|||>3<|||>1
+Servers\localhost\ServerVersionFull<|||>1<|||>8.0.30 - MySQL Community Server - GPL
+Servers\localhost\ConnectCount<|||>3<|||>2
+Servers\localhost\ServerVersion<|||>3<|||>80030
+Servers\localhost\LastConnect<|||>1<|||>2024-11-03 18:37:22
+Servers\localhost\lastUsedDB<|||>1<|||>gaulois
+Servers\localhost\FavoriteObjects<|||>1<|||>gaulois||personnage<{{{><}}}>
+Servers\localhost\QueryHistory\1<|||>1<|||>2024-11-03 19:00:08|gaulois|16|SELECT nom_lieu FROM lieu<{{{><}}}>WHERE nom_lieu LIKE "%um";<{{{><}}}>
+Servers\localhost\QueryHistory\2<|||>1<|||>2024-11-03 19:06:49|gaulois|0|SELECT COUNT(nom_personnage), nom_lieu <{{{><}}}>FROM personnage p, lieu l<{{{><}}}>WHERE p.id_lieu = l.id_lieu<{{{><}}}>GROUP BY nom_lieu<{{{><}}}>ORDER BY nom_lieu DESC;<{{{><}}}>
+Servers\localhost\QueryHistory\3<|||>1<|||>2024-11-03 19:08:57|gaulois|0|SELECT COUNT(nom_personnage), nom_lieu <{{{><}}}>FROM personnage p, lieu l<{{{><}}}>WHERE p.id_lieu = l.id_lieu<{{{><}}}>GROUP BY nom_lieu<{{{><}}}><{{{><}}}>
+Servers\localhost\QueryHistory\4<|||>1<|||>2024-11-03 19:09:50|gaulois|16|SELECT nom_personnage, nom_specialite, adresse_personnage, nom_lieu<{{{><}}}>FROM lieu l, personnage p, specialite s<{{{><}}}>WHERE l.id_lieu = p.id_lieu<{{{><}}}>AND s.id_specialite = p.id_specialite<{{{><}}}>ORDER BY nom_lieu, nom_personnage; <{{{><}}}>
+Servers\localhost\QueryHistory\5<|||>1<|||>2024-11-03 19:15:46|gaulois|16|SELECT COUNT(nom_personnage), nom_specialite <{{{><}}}>FROM personnage p, specialite s<{{{><}}}>WHERE p.id_specialite = s.id_specialite<{{{><}}}>GROUP BY nom_specialite<{{{><}}}>ORDER BY COUNT(nom_personnage) DESC;<{{{><}}}>
+Servers\localhost\QueryHistory\6<|||>1<|||>2024-11-03 20:51:42|gaulois|32|SELECT nom_potion, SUM(cout_ingredient) AS total<{{{><}}}>FROM potion p, ingredient i, composer c<{{{><}}}>WHERE p.id_potion = c.id_potion<{{{><}}}>AND i.id_ingredient = c.id_ingredient<{{{><}}}>GROUP BY p.nom_potion<{{{><}}}>ORDER BY total DESC;<{{{><}}}><{{{><}}}>
+Servers\localhost\QueryHistory\7<|||>1<|||>2024-11-03 21:00:14|gaulois|15|SELECT nom_personnage, dose_boire<{{{><}}}>FROM personnage p, boire b<{{{><}}}>WHERE p.id_personnage = b.id_personnage<{{{><}}}>AND dose_boire = 0;
+Servers\localhost\QueryHistory\8<|||>1<|||>2024-11-03 21:00:37|gaulois|0|SELECT nom_lieu, COUNT(nom_personnage) AS total<{{{><}}}>FROM lieu l, personnage p<{{{><}}}>WHERE l.id_lieu = p.id_lieu<{{{><}}}>AND nom_lieu != "Village gaulois"<{{{><}}}>GROUP BY nom_lieu<{{{><}}}>ORDER BY total DESC<{{{><}}}>LIMIT 2;
+Servers\localhost\QueryHistory\9<|||>1<|||>2024-11-03 21:00:49|gaulois|0|SELECT nom_potion, nom_ingredient<{{{><}}}>FROM potion p, ingredient i, composer c<{{{><}}}>WHERE p.id_potion = c.id_potion<{{{><}}}>AND i.id_ingredient = c.id_ingredient<{{{><}}}>AND nom_ingredient = "Poisson frais";
+Servers\localhost\QueryHistory\10<|||>1<|||>2024-11-03 21:01:02|gaulois|31|SELECT nom_type_casque, COUNT(c.id_type_casque) AS nb, SUM(cout_casque)<{{{><}}}>FROM type_casque t, casque c<{{{><}}}>WHERE t.id_type_casque = c.id_type_casque<{{{><}}}>GROUP BY nom_type_casque<{{{><}}}>ORDER BY nb DESC;
+Servers\localhost\QueryHistory\11<|||>1<|||>2024-11-03 21:03:07|gaulois|0|SELECT nom_ingredient, cout_ingredient, qte, nom_potion<{{{><}}}>FROM ingredient i, potion p, composer c<{{{><}}}>WHERE i.id_ingredient = c.id_ingredient<{{{><}}}>AND p.id_potion = c.id_potion<{{{><}}}>AND nom_potion = "Santé";<{{{><}}}>
